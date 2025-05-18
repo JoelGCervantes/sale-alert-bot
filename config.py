c0a0f11a58c4e26214d9991227324a6ee9ba0421
Joel Garcia-Cervantes <garciac2@babbage.cs.pdx.edu>
2025-05-18 13:10:18 -0700

    Stop tracking config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-# DO NOT COMMIT THIS FILE
-
-DISCORD_TOKEN = "YOUR_DISCORD_BOT_TOKEN"
-CHANNEL_ID = 1373750115956822226  # Replace with your discord channel ID
-TARGET_URL = "https://www.nordstrom.com/browse/sale/men/clothing?breadcrumb=Home%2FSale%2FMen%2FClothing&origin=topnav&postalCodeAvailability=97201&filterByBrand=psycho-bunny"  # URL to check
-CHECK_INTERVAL = 30  # in minutes
