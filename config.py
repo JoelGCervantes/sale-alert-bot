7a528a7c620153cef289d9249704d4cb8a4ea8e8
Joel Garcia-Cervantes <garciac2@babbage.cs.pdx.edu>
2025-05-18 13:07:50 -0700

    testing git ignore

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # config.py
+# DO NOT COMMIT THIS FILE
 
 DISCORD_TOKEN = "YOUR_DISCORD_BOT_TOKEN"
-CHANNEL_ID = 123456789012345678  # Replace with your channel ID
-TARGET_URL = "https://example.com/sale"  # URL to check
+CHANNEL_ID = 1373750115956822226  # Replace with your discord channel ID
+TARGET_URL = "https://www.nordstrom.com/browse/sale/men/clothing?breadcrumb=Home%2FSale%2FMen%2FClothing&origin=topnav&postalCodeAvailability=97201&filterByBrand=psycho-bunny"  # URL to check
 CHECK_INTERVAL = 30  # in minutes
