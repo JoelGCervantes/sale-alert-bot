ef383bbd548f01613f65cb22c608c0cd69eb8095
Joel Garcia-Cervantes <garciac2@babbage.cs.pdx.edu>
2025-05-18 13:14:50 -0700

    example config file

---
+++ config_example.py
@@ -0,0 +1,7 @@
+# config.py
+# DO NOT COMMIT THIS FILE
+
+DISCORD_TOKEN = "YOUR_DISCORD_BOT_TOKEN"
+CHANNEL_ID = 123456789  # Replace with your discord channel ID
+TARGET_URL = "https://example.com"  # URL to check
+CHECK_INTERVAL = 30  # in minutes
